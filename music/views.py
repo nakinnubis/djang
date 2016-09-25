cc15a893dfe9429ce768e1a9696b66b851eb3bb3
nakinnubis <abiola.akinnubi@gmail.com>
2016-09-25 04:25:39 +0100

    Views is been added
    Views gradully been added

--- music/views.py
+++ music/views.py
@@ -5,8 +5,11 @@ from .models import Albums
 #this is define based on the url that i want for my default url. it is the same as the name=index
 def index(request):
     all_albums = Albums.objects.all()
-    template = loader.get_template(music/index.html)    
-    return HttpResponse(html)
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, albums_id):
     return HttpResponse("<h1> Welcome retrieving album details" + str(albums_id) + "</h1>" )
